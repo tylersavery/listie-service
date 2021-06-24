a850b913222ab9b647b79640d0a43cc2fd4bd5a9
Tyler Savery <tyler@theyoungastronauts.com>
2021-06-24 19:54:51 -0400

    psql database fix

--- app/database.py
+++ app/database.py
@@ -6,7 +6,10 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
 
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL.replace("postgres://", "postgresql://"),
+    connect_args={"check_same_thread": False},
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

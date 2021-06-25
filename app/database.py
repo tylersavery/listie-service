921f31bcfa46d956abc42fdaab699bae9f2f0523
Tyler Savery <tyler@theyoungastronauts.com>
2021-06-24 20:01:25 -0400

    environment var

--- app/database.py
+++ app/database.py
@@ -4,11 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
-
+ENVIRONMENT = os.getenv("ENVIRONMENT", "dev")
 
 engine = create_engine(
     DATABASE_URL.replace("postgres://", "postgresql://"),
-    # connect_args={"check_same_thread": False},
+    connect_args={"check_same_thread": False} if ENVIRONMENT == "dev" else {},
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
